3ef82686110855113abbf98d8c7a9193f4af2bd4
Daniel <danielthecrosscarier@gmail.com>
2021-03-01 23:35:07 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+import gulp from 'gulp'; const { src, dest, watch, series, parallel } = gulp
+import del from 'del'
+import include from 'gulp-file-include'
+import htmlmin from 'gulp-htmlmin'
+import autoPrefixer from 'gulp-autoprefixer'
+import concat from 'gulp-concat'
+import pxtorem from 'gulp-pxtorem'
+import csso from 'gulp-csso'
+import replace from 'gulp-replace'
+import babel from 'gulp-babel'
+import uglify from 'gulp-uglify'
+import { stream, init, reload, create } from 'browser-sync'
+
+const html = () => src('src/**/*.html')
+  .pipe(include())
+  .pipe(htmlmin({
+    collapseWhitespace: true
+  }))
+  .pipe(dest('dist'))
+  .pipe(stream())
+
+const css = () => src([
+  'src/css/preset.css',
+  // 'src/css/variables.css',
+  'src/css/fonts.css',
+  'src/css/style.css',
+  'src/css/media.css',
+  'src/css/**/*.css'
+])
+  .pipe(concat('style.css'))
+  .pipe(autoPrefixer())
+  .pipe(pxtorem({
+    rootValue: 12,
+    unitPrecision: 4,
+    propList: ['*']
+  }))
+  .pipe(replace(/Px|PX|pX/g, 'px'))
+  .pipe(csso())
+  .pipe(dest('dist/css'))
+  .pipe(stream())
+
+const js = () => src('src/js/**/*.js')
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(concat('script.js'))
+  .pipe(uglify())
+  .pipe(dest('dist/js'))
+  .pipe(stream())
+
+const img = () => src('src/img/**/*')
+  .pipe(dest('dist/img'))
+  .pipe(stream())
+
+const fonts = () => src('src/fonts/**/*')
+  .pipe(dest('dist/fonts'))
+  .pipe(stream())
+
+const serve = () => {
+  create()
+  init({
+    server: {
+      baseDir: 'dist'
+    }
+  })
+
+  watch('src/**/*.html', html).on('change', reload)
+  watch('src/css/**/*.css', css)
+  watch('src/js/**/*.js', js)
+  watch('src/img/**/*', img)
+  watch('src/fonts/**/*', fonts)
+}
+
+export const clean = () => del('dist')
+
+export default series(
+  clean,
+  parallel(css, js, img, fonts, html),
+  serve
+)
+
+export const build = series(
+  clean,
+  parallel(css, js, img, fonts, html)
+)
---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+/* Contacts spoiler */
+
+const
+  contactsBtn = document.querySelector('.contacts__spoiler-btn'),
+  contacts = document.querySelector('.header__contacts'),
+  supHeader = document.querySelector('.header__sup')
+
+const supHeaderInitialHeight = window.getComputedStyle(supHeader, null)
+  .getPropertyValue('height')
+
+contactsBtn.onclick = () => {
+  
+  contactsBtn.classList.toggle('contacts__spoiler-btn_active')
+
+  if (contacts.classList.contains('contacts_visible')) {
+    supHeader.style.height = supHeaderInitialHeight
+  }
+  else {
+    supHeader.style.height = 
+      +supHeaderInitialHeight.replace('px', '') 
+      + contacts.scrollHeight + 'px'
+  }
+
+}
+
+/* Burger-menu */
+
+const
+  burgerBtn = document.querySelector('.navbar__burger-btn'),
+  nav = document.querySelector('.navbar__nav')
+
+burgerBtn.onclick = () => {
+
+  burgerBtn.classList.toggle('navbar__burger-btn_active')
+
+  document.body.classList.toggle('no-overflow')
+
+}
