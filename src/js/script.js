959bbfbffebee98d563405c05adb8d22edf8c6e0
Daniel <danielthecrosscarier@gmail.com>
2021-03-02 11:56:32 +0300

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,40 @@
-/* Contacts spoiler */
+document.addEventListener('DOMContentLoaded', () => {
+  /* Contacts spoiler */
 
-const
-  contactsBtn = document.querySelector('.contacts__spoiler-btn'),
-  contacts = document.querySelector('.header__contacts'),
-  supHeader = document.querySelector('.header__sup')
+  const
+    contactsBtn = document.querySelector('.contacts__spoiler-btn'),
+    contacts = document.querySelector('.header__contacts'),
+    supHeader = document.querySelector('.header__sup')
 
-const supHeaderInitialHeight = window.getComputedStyle(supHeader, null)
-  .getPropertyValue('height')
+  const supHeaderInitialHeight = window.getComputedStyle(supHeader, null)
+    .getPropertyValue('height')
 
-contactsBtn.onclick = () => {
-  
-  contactsBtn.classList.toggle('contacts__spoiler-btn_active')
+  contactsBtn.onclick = () => {
+    
+    contactsBtn.classList.toggle('contacts__spoiler-btn_active')
 
-  if (contacts.classList.contains('contacts_visible')) {
-    supHeader.style.height = supHeaderInitialHeight
+    if (contacts.classList.contains('contacts_visible')) {
+      supHeader.style.height = supHeaderInitialHeight
+    }
+    else {
+      supHeader.style.height = 
+        +supHeaderInitialHeight.replace('px', '') 
+        + contacts.scrollHeight + 'px'
+    }
+
   }
-  else {
-    supHeader.style.height = 
-      +supHeaderInitialHeight.replace('px', '') 
-      + contacts.scrollHeight + 'px'
+
+  /* Burger-menu */
+
+  const
+    burgerBtn = document.querySelector('.navbar__burger-btn'),
+    nav = document.querySelector('.navbar__nav')
+
+  burgerBtn.onclick = () => {
+
+    burgerBtn.classList.toggle('navbar__burger-btn_active')
+
+    document.body.classList.toggle('no-overflow')
+
   }
-
-}
-
-/* Burger-menu */
-
-const
-  burgerBtn = document.querySelector('.navbar__burger-btn'),
-  nav = document.querySelector('.navbar__nav')
-
-burgerBtn.onclick = () => {
-
-  burgerBtn.classList.toggle('navbar__burger-btn_active')
-
-  document.body.classList.toggle('no-overflow')
-
-}
+})
